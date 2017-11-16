ec85dfecb6de6fc345c72bafeaf8b4125b4ca37f
Marius van Niekerk <marius.v.niekerk@gmail.com>
2017-11-15 22:53:19 -0500

    Try disable compression for lazy load module

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 REM Don't build html docs for this since for some weird reason that fails
-"%R%" CMD INSTALL --build --no-html .
+"%R%" CMD INSTALL --build --no-html --data-compress=none .
 if errorlevel 1 exit 1
 
 mkdir "%PREFIX%\share\jupyter\kernels\ir"
