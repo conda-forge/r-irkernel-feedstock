e30f5b380294348b7f229c7c0d62ff7171f8a229
Daniel Bast <2790401+dbast@users.noreply.github.com>
2018-11-28 08:17:35 +0100

    Update recipe

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,9 @@
 REM Don't build html docs for this since for some weird reason that fails
 "%R%" CMD INSTALL --build --no-html --data-compress=none .
-if errorlevel 1 exit 1
+IF %ERRORLEVEL% NEQ 0 exit 1
 
 mkdir "%PREFIX%\share\jupyter\kernels\ir"
-if errorlevel 1 exit 1
+IF %ERRORLEVEL% NEQ 0 exit 1
 
 xcopy /s inst\kernelspec\* "%PREFIX%\share\jupyter\kernels\ir\"
-if errorlevel 1 exit 1
+IF %ERRORLEVEL% NEQ 0 exit 1
