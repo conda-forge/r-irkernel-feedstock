4c602933f78490a609f50743b73e10301a9de885
Philip Khor <35039795+philip-khor@users.noreply.github.com>
2022-10-13 23:57:41 +0800

    use IRkernel::installspec directly for build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,8 +2,5 @@ REM Don't build html docs for this since for some weird reason that fails
 "%R%" CMD INSTALL --build --no-html --data-compress=none .
 IF %ERRORLEVEL% NEQ 0 exit 1
 
-mkdir "%PREFIX%\share\jupyter\kernels\ir"
-IF %ERRORLEVEL% NEQ 0 exit 1
-
-xcopy /s inst\kernelspec\* "%PREFIX%\share\jupyter\kernels\ir\"
+"%R%" -e "IRkernel::installspec(prefix = %PREFIX%)"
 IF %ERRORLEVEL% NEQ 0 exit 1
