3f506e0cdb28d06bccb5ccc1bcfff91c56f2992f
Philip Khor <35039795+philip-khor@users.noreply.github.com>
2022-10-16 00:41:23 +0800

    drop comments

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,5 @@
 export DISABLE_AUTOBREW=1
 $R CMD INSTALL --build .
 
-# This installs to the wrong place (/usr/local)
-# $R -e "IRkernel::installspec(user=F)"
-
 mkdir -p $PREFIX/share/jupyter/kernels/ir
 cp inst/kernelspec/* $PREFIX/share/jupyter/kernels/ir/
