5da41fb2afad0a0f9e7e175e5e670d29925a6b7a
Philip Khor <35039795+philip-khor@users.noreply.github.com>
2022-10-15 23:29:21 +0800

    fully restore previous behaviour

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,9 @@
 REM Don't build html docs for this since for some weird reason that fails
 "%R%" CMD INSTALL --build --no-html --data-compress=none .
 IF %ERRORLEVEL% NEQ 0 exit 1
+
+mkdir "%PREFIX%\share\jupyter\kernels\ir"
+IF %ERRORLEVEL% NEQ 0 exit 1
+
+xcopy /s inst\kernelspec\* "%PREFIX%\share\jupyter\kernels\ir\"
+IF %ERRORLEVEL% NEQ 0 exit 1
